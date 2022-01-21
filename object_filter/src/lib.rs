0a03c9bf7c33337343a4aedc07cf54cc2f5ea64c
mohit0928 <mohityadav0928@gmail.com>
2022-01-22 02:14:54 +0530

    Michael made the code more efficient

--- object_filter/src/lib.rs
+++ object_filter/src/lib.rs
@@ -1,18 +1,21 @@
 #![no_std]
 
 extern crate alloc;
-use alloc::{sync::Arc, vec, vec::Vec};
+use core::cmp::Ordering;
+
+use alloc::vec::Vec;
 use hotg_rune_proc_blocks::{ProcBlock, Tensor, Transform};
 use libm::fabsf;
 
-/// A proc-block which will take [1,*,85] dimension where
-/// * is no. of prediction from the models
-/// 85 -> [x-cordinate, y-cordinate, width, height, confidence_value,
-/// pred_classes(80 COCO classes)]  -> 5 + 80 COCO classes and will change it to
-/// [*,6] here * is the number of prediction in the image who have a confidence
-/// value above threshold and 6 refer to [ x-coordinate, y-coordinate, h, w,
-/// confidence value, label_index]
-
+/// A proc-block which takes 3-d tensor `[1, num_detection, detection_box(x, y,
+/// w, h) + confidence_scores + total_detection_classes]` and filter the
+/// detected objects to:
+/// 1. remove duplicate detection for a single object
+/// 2. remove the objects with low confidence based on a threshold
+///
+/// giving a 2-d tensor with dimension `[*, 6]` (where * is total number of
+/// detected objects,  and 6 -> `[ x-coordinate, y-coordinate, h, w,
+/// confidence_value, label_index]`) as output.
 #[derive(Debug, Clone, PartialEq, ProcBlock)]
 pub struct ObjectFilter {
     threshold: f32,
@@ -31,86 +34,112 @@ impl Transform<Tensor<f32>> for ObjectFilter {
 
     fn transform(&mut self, input: Tensor<f32>) -> Tensor<f32> {
         let dim = input.dimensions();
-        let input: Vec<f32> = input.elements().iter().map(|&x| x).collect();
-
-        let mut vec_2d: Vec<Vec<f32>> = Vec::new();
-
-        let mut j = 0;
-        let dimensions = 85; // [cx, cy, w, h, conf, pred_cls(80)] -> 85 (5 + 80 classes)
-        let confidence_index = 4;
-        let label_start_index = 5;
-
-        // loop through all the objects and select only those objects which have
-        // confidence value above threshold
-
-        for i in 0..dim[1] {
-            let index = i * dimensions;
-
-            if j != 0 {
-                let rows = vec_2d.len();
-                let mut x = 0;
-                for i in 0..rows {
-                    if fabsf(input[index] - vec_2d[i][0]) <= 0.01
-                        && fabsf(input[index + 1] - vec_2d[i][1]) <= 0.01
-                    {
-                        x = 1;
-                        continue;
-                    }
-                }
-                if x == 1 {
-                    continue;
-                }
-            }
-            if input[index + confidence_index] <= self.threshold {
-                continue;
-            }
-
-            let (ind, value) = &input
-                [index + label_start_index..index + dimensions]
-                .iter()
-                .enumerate()
-                .fold(
-                    (0, 0.0),
-                    |max, (ind, &val)| {
-                        if val > max.1 {
-                            (ind, val)
-                        } else {
-                            max
-                        }
-                    },
-                );
-
-            if value <= &self.threshold {
-                continue;
-            }
-            vec_2d.push(vec![]);
-            vec_2d[j].push(input[index]); // x-coordinate
-            vec_2d[j].push(input[index + 1]); // y-coordinate
-            vec_2d[j].push(input[index + 2]); // h
-            vec_2d[j].push(input[index + 3]); // w
-            vec_2d[j].push(*value as f32); // label confidence values
-            vec_2d[j].push(*ind as f32); // label index
-
-            j = j + 1;
-        }
-
-        let rows = vec_2d.len();
-        let columns = vec_2d[0].len();
-
-        let elements: Arc<[f32]> = vec_2d
-            .into_iter()
-            .flat_map(|v: Vec<f32>| v.into_iter())
+        let rectangles = input.view::<3>().expect("a 3-d tensor");
+        let mut objects: Vec<Object> = (0..dim[1])
+            .map(|object_index| {
+                rectangles.slice::<1>(&[0, object_index]).unwrap()
+            })
+            .filter(|view| view[4] > self.threshold)
+            .map(|view| Object::from_row(view.elements()))
             .collect();
 
-        Tensor::new_row_major(elements, alloc::vec![rows, columns])
+        while let Some((first, second)) = find_duplicate(&objects) {
+            if objects[first].confidence > objects[second].confidence {
+                objects.remove(second);
+            } else {
+                objects.remove(first);
+            }
+        }
+
+        let rows = objects.len();
+        let elements = objects
+            .into_iter()
+            .flat_map(|j| j.into_elements())
+            .collect();
+
+        Tensor::new_row_major(elements, alloc::vec![rows, 6])
     }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Object {
+    x: f32,
+    y: f32,
+    height: f32,
+    width: f32,
+    confidence: f32,
+    index: usize,
+}
+
+impl Object {
+    pub fn from_row(slice: &[f32]) -> Self {
+        match *slice {
+            [x, y, height, width, _, ref labels @ ..] => {
+                let (index, confidence) = labels
+                    .iter()
+                    .copied()
+                    .enumerate()
+                    .max_by(|a, b| {
+                        if a.1 < b.1 {
+                            Ordering::Less
+                        } else if a.1 > b.1 {
+                            Ordering::Greater
+                        } else {
+                            Ordering::Equal
+                        }
+                    })
+                    .expect("there should be at least one label");
+
+                Object {
+                    x,
+                    y,
+                    height,
+                    width,
+                    confidence,
+                    index,
+                }
+            },
+            _ => panic!("expected at least 5 elements"),
+        }
+    }
+
+    fn is_duplicated(&self, other: &Object, threshold: f32) -> bool {
+        fabsf(self.x - other.x) <= threshold
+            && fabsf(self.y - other.y) <= threshold
+    }
+
+    fn into_elements(self) -> impl IntoIterator<Item = f32> {
+        let Object {
+            x,
+            y,
+            height,
+            width,
+            confidence,
+            index,
+        } = self;
+        [x, y, height, width, confidence, index as f32]
+    }
+}
+
+fn find_duplicate(objects: &[Object]) -> Option<(usize, usize)> {
+    for i in 0..objects.len() {
+        for j in i + 1..objects.len() {
+            if objects[i].is_duplicated(&objects[j], 0.01) {
+                return Some((i, j));
+            }
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
 
 mod test {
     use super::*;
+    use alloc::vec;
     #[test]
+
     fn test_object_filter() {
         let v: Tensor<f32> = [[[
             0.27335986, 0.43181776, 0.40072349, 0.33026114, 0.75, 0.1849257,
@@ -139,5 +168,22 @@ mod test {
         ]]
         .into();
         assert_eq!(output, should_be);
+    }
+
+    #[test]
+    fn find_the_duplicates() {
+        let obj = Object {
+            x: 0.5,
+            y: 0.5,
+            width: 1.0,
+            height: 1.0,
+            confidence: 1.0,
+            index: 0,
+        };
+        let objects = vec![obj, obj];
+
+        let duplicate_indices = find_duplicate(&objects).unwrap();
+
+        assert_eq!(duplicate_indices, (0, 1));
     }
 }
