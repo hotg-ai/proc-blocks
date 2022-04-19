471d4635c5bb72552048ad448a28f4251fd0ef7e
Jacek Pierzchlewski <jacek@hotg.ai>
2022-04-20 02:29:32 +1000

    Softmax is migrated (wip)

--- softmax/src/lib.rs
+++ softmax/src/lib.rs
@@ -1,82 +1,134 @@
-#![cfg_attr(not(feature = "metadata"), no_std)]
+use crate::{
+    proc_block_v1::{ GraphError, KernelError, InvalidInput, BadInputReason},
+    runtime_v1::*,
+};
 
-extern crate alloc;
+use hotg_rune_proc_blocks::{
+    ndarray::ArrayViewMut1, BufferExt, ValueType,
+};
 
-use hotg_rune_proc_blocks::{ProcBlock, Tensor, Transform};
-use libm::expf;
+use num_traits::Float;
 
-#[derive(Debug, Clone, PartialEq, ProcBlock)]
-pub struct Softmax {}
+wit_bindgen_rust::import!("../wit-files/rune/runtime-v1.wit");
+wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
 
-impl Softmax {
-    pub const fn new() -> Self { Softmax {} }
-}
+struct ProcBlockV1;
 
-impl Default for Softmax {
-    fn default() -> Self { Softmax::new() }
-}
 
-impl Transform<Tensor<f32>> for Softmax {
-    type Output = Tensor<f32>;
-
-    fn transform(&mut self, input: Tensor<f32>) -> Tensor<f32> {
-        let b = input.map(|_, &x| expf(x as f32));
-        let sum: f32 = b.elements().iter().sum();
-
-        b.map(|_, &x| x / sum)
-    }
-}
-
-#[cfg(feature = "metadata")]
-pub mod metadata {
-    wit_bindgen_rust::import!(
-        "../wit-files/rune/runtime-v1.wit"
-    );
-    wit_bindgen_rust::export!(
-        "../wit-files/rune/rune-v1.wit"
-    );
-
-    struct RuneV1;
-
-    impl rune_v1::RuneV1 for RuneV1 {
-        fn start() {
-            use runtime_v1::*;
-
-            let metadata = Metadata::new("Softmax", env!("CARGO_PKG_VERSION"));
-            metadata.set_description("Apply [softmax](https://en.wikipedia.org/wiki/Softmax_function), sometimes referred to as the *\"normalized exponential function\"* to each element in the tensor.");
-            metadata.set_repository(env!("CARGO_PKG_REPOSITORY"));
-            metadata.set_homepage(env!("CARGO_PKG_HOMEPAGE"));
-
-            let input = TensorMetadata::new("input");
-            let hint =
-                supported_shapes(&[ElementType::Float32], Dimensions::Dynamic);
-            input.add_hint(&hint);
-            metadata.add_input(&input);
-
-            let output = TensorMetadata::new("output");
-            output.set_description("The result of `exp(x)` to each element, `x`, in the tensor and dividing by the sum.");
-            let hint =
-                supported_shapes(&[ElementType::Float32], Dimensions::Dynamic);
-            output.add_hint(&hint);
-            metadata.add_output(&output);
-
-            register_node(&metadata);
+fn softmax<T>(mut input:  ArrayViewMut1<T>) where T: Float + num_traits::FromPrimitive {
+        input.mapv_inplace(|x| x.exp());
+        match input.mean() {
+            Some(sum) => input.mapv_inplace(|x| x / sum),
+            _  => {}
         }
-    }
+ }
+ 
+
+
+fn preprocess_buffer<'buf, T>(
+    buffer: &'buf mut[u8],
+    dimensions: &[u32],
+) -> Result<ArrayViewMut1<'buf, T>, KernelError>
+where
+    T: ValueType,
+{
+    buffer
+        .view_mut::<T>(dimensions)
+        .and_then(|t| t.into_dimensionality())
+        .map_err(|e| {
+            KernelError::InvalidInput(InvalidInput{
+                name: "confidences".to_string(),
+                reason: BadInputReason::InvalidValue(e.to_string()),
+            })
+        })
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use alloc::vec;
 
-    #[test]
-    fn test_softmax() {
-        let v = Tensor::new_vector(vec![2.3, 12.4, 5.1]);
-        let mut softmax = Softmax::default();
-        let output = softmax.transform(v);
-        let should_be =
-            Tensor::new_vector(vec![0.000041050153, 0.99928397, 0.00067505526]);
-        assert_eq!(output, should_be);
+impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
+    fn register_metadata(){
+        let metadata = Metadata::new("Softmax", env!("CARGO_PKG_VERSION"));
+        metadata.set_description(env!("CARGO_PKG_DESCRIPTION"));
+        metadata.set_repository(env!("CARGO_PKG_REPOSITORY"));
+        metadata.set_homepage(env!("CARGO_PKG_HOMEPAGE"));
+        metadata.add_tag("softmax");
+        metadata.add_tag("image");
+        metadata.add_tag("nlp");
+        metadata.add_tag("numeric");
+        metadata.add_tag("classification");
+
+        let input = TensorMetadata::new("input");
+        let hint =
+            supported_shapes(&[ElementType::F32, ElementType::F64], Dimensions::Fixed(&[0]));
+        input.add_hint(&hint);
+        metadata.add_input(&input);
+
+        let soft_max = TensorMetadata::new("soft_max");
+        soft_max.set_description("Vector normalised into probability distribution");
+        let hint =
+            supported_shapes(&[ElementType::F32, ElementType::F64], Dimensions::Fixed(&[0]));
+        soft_max.add_hint(&hint);
+        metadata.add_output(&soft_max);
+
+        register_node(&metadata);
+    }
+
+    fn graph(id: String)-> Result<(), GraphError> {
+        let ctx = GraphContext::for_node(&id).ok_or_else(|| {
+            GraphError::Other("Unable to get the graph context".to_string())
+        })?;
+
+        ctx.add_input_tensor(
+            "input",
+            ElementType::F32,
+            Dimensions::Dynamic,
+        );
+
+        ctx.add_output_tensor(
+            "soft_max",
+            ElementType::F32,
+            Dimensions::Dynamic,
+        );
+
+        Ok(())
+    }
+
+  
+    fn kernel(id: String)-> Result<(), KernelError> {
+        let ctx = KernelContext::for_node(&id).ok_or_else(|| {
+            KernelError::Other("Unable to get the kernel context".to_string())
+        })?;
+
+        let TensorResult {
+            element_type,
+            dimensions,
+            mut buffer,
+        } = ctx.get_input_tensor("input").ok_or_else(|| {
+            KernelError::InvalidInput(InvalidInput {
+                name: "input".to_string(),
+                reason: BadInputReason::NotFound,
+            })
+        })?;
+
+        match element_type {
+            ElementType::F32 => preprocess_buffer::<f32>(&mut buffer, &dimensions).map(|t| softmax(t))?,
+            ElementType::F64 => preprocess_buffer::<f64>(&mut buffer, &dimensions).map(|t| softmax(t))?,
+            other => {
+                return Err(KernelError::Other(format!(
+                "The softmax proc-block only accepts f32 or f64 tensors, found {:?}",
+                other,
+                )))
+            },
+        };
+ 
+        ctx.set_output_tensor(
+            "soft_max",
+            TensorParam {
+                element_type,
+                dimensions: &dimensions,
+                buffer: &buffer,
+            },
+        );
+
+        Ok(())
     }
 }
