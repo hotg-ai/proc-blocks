0f8acb2fceef2c28ab2c5b8baed309f6c0bcdc94
Kartik Thakore <kthakore@alumni.uwo.ca>
2022-04-25 17:07:31 -0700

    Adding stats proc-block

---
+++ stats/src/lib.rs
@@ -0,0 +1,83 @@
+use crate::{
+    proc_block_v1::{GraphError, KernelError},
+    runtime_v1::{
+        Dimensions, ElementType, GraphContext, KernelContext, Metadata, TensorMetadata,
+        TensorParam, TensorResult,
+    },
+};
+use hotg_rune_proc_blocks::{ndarray::ArrayView1, BufferExt, SliceExt};
+
+wit_bindgen_rust::import!("../wit-files/rune/runtime-v1.wit");
+wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
+
+struct ProcBlockV1;
+
+impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
+    fn register_metadata() {
+        let meta = Metadata::new("Statistics", env!("CARGO_PKG_VERSION"));
+
+        let samples = TensorMetadata::new("samples");
+        samples.set_description("All samples to perform an average on");
+        meta.add_input(&samples);
+
+        let mean = TensorMetadata::new("mean");
+        mean.set_description("The mean");
+        meta.add_input(&mean);
+
+        let std_dev = TensorMetadata::new("std_dev");
+        std_dev.set_description("The standard deviation.");
+        meta.add_input(&std_dev);
+
+        runtime_v1::register_node(&meta);
+    }
+
+    fn graph(id: String) -> Result<(), GraphError> {
+        let ctx = GraphContext::for_node(&id).unwrap();
+
+        ctx.add_input_tensor("samples", ElementType::F64, Dimensions::Fixed(&[0]));
+        ctx.add_output_tensor("mean", ElementType::F64, Dimensions::Fixed(&[1]));
+        ctx.add_output_tensor("std_dev", ElementType::F64, Dimensions::Fixed(&[1]));
+
+        Ok(())
+    }
+
+    fn kernel(id: String) -> Result<(), KernelError> {
+        let ctx = KernelContext::for_node(&id).unwrap();
+
+        let TensorResult {
+            element_type,
+            buffer,
+            dimensions,
+        } = ctx.get_input_tensor("samples").unwrap();
+
+        let samples: ArrayView1<f64> = match element_type {
+            ElementType::F64 => buffer
+                .view(&dimensions)
+                .unwrap()
+                .into_dimensionality()
+                .unwrap(),
+            _ => panic!("Handle invalid element type"),
+        };
+        let mean = samples.mean().unwrap();
+        let std_dev = samples.std(1.0);
+
+        ctx.set_output_tensor(
+            "mean",
+            TensorParam {
+                element_type: ElementType::F64,
+                dimensions: &[1],
+                buffer: [mean].as_bytes(),
+            },
+        );
+        ctx.set_output_tensor(
+            "std_dev",
+            TensorParam {
+                element_type: ElementType::F64,
+                dimensions: &[1],
+                buffer: [std_dev].as_bytes(),
+            },
+        );
+
+        Ok(())
+    }
+}
