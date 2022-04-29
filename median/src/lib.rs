bb396ec1b03edccc54d36f881312cac0343bd39d
f0rodo <kthakorefi@gmail.com>
2022-04-29 05:15:57 +0000

    Fixed median and argmax

--- median/src/lib.rs
+++ median/src/lib.rs
@@ -1,11 +1,11 @@
 use crate::{
     proc_block_v1::{GraphError, KernelError},
     runtime_v1::{
-        Dimensions, ElementType, GraphContext, KernelContext, Metadata, TensorMetadata,
-        TensorParam, TensorResult, supported_shapes
+        supported_shapes, Dimensions, ElementType, GraphContext, KernelContext,
+        Metadata, TensorMetadata, TensorParam, TensorResult,
     },
 };
-use hotg_rune_proc_blocks::{ndarray::ArrayView1, BufferExt, SliceExt};
+use hotg_rune_proc_blocks::{ndarray::ArrayViewMut1, BufferExt, SliceExt};
 
 wit_bindgen_rust::import!("../wit-files/rune/runtime-v1.wit");
 wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
@@ -21,18 +21,16 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
         metadata.add_tag("numeric");
         metadata.add_tag("stats");
         let samples = TensorMetadata::new("samples");
-        samples.set_description("All samples to perform an average on");
+        samples.set_description("All samples to perform an median on.");
         let hint = supported_shapes(&[ElementType::F64], Dimensions::Dynamic);
         samples.add_hint(&hint);
         metadata.add_input(&samples);
-        
 
         let median = TensorMetadata::new("median");
         median.set_description("The median");
         let hint = supported_shapes(&[ElementType::F64], Dimensions::Dynamic);
         median.add_hint(&hint);
         metadata.add_output(&median);
-        
 
         runtime_v1::register_node(&metadata);
     }
@@ -40,8 +38,16 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
     fn graph(id: String) -> Result<(), GraphError> {
         let ctx = GraphContext::for_node(&id).unwrap();
 
-        ctx.add_input_tensor("samples", ElementType::F64, Dimensions::Fixed(&[0]));
-        ctx.add_output_tensor("median", ElementType::F64, Dimensions::Fixed(&[1]));
+        ctx.add_input_tensor(
+            "samples",
+            ElementType::F64,
+            Dimensions::Fixed(&[0]),
+        );
+        ctx.add_output_tensor(
+            "median",
+            ElementType::F64,
+            Dimensions::Fixed(&[1]),
+        );
 
         Ok(())
     }
@@ -51,29 +57,37 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
 
         let TensorResult {
             element_type,
-            buffer,
+            mut buffer,
             dimensions,
         } = ctx.get_input_tensor("samples").unwrap();
 
-        let samples: ArrayView1<f64> = match element_type {
+        let mut samples: ArrayViewMut1<f64> = match element_type {
             ElementType::F64 => buffer
-                .view(&dimensions)
+                .view_mut(&dimensions)
                 .unwrap()
                 .into_dimensionality()
                 .unwrap(),
             _ => panic!("Handle invalid element type"),
         };
-        let mut median_slice: Vec<f64> = samples.to_slice().unwrap().to_vec();
-            median_slice.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        samples
+            .as_slice_mut()
+            .unwrap()
+            .sort_by(|a, b| a.partial_cmp(b).unwrap());
 
-        let median = (median_slice.len() as f32 / 2.0) as i32 as usize; 
+        let median = if samples.len() % 2 == 1 {
+            let idx = (samples.len() / 2);
+            samples[idx]
 
+        } else {
+            let idx = (samples.len() / 2);
+            (samples[idx] + samples[idx+1]) / 2.0
+        };
         ctx.set_output_tensor(
             "median",
             TensorParam {
                 element_type: ElementType::F64,
                 dimensions: &[1],
-                buffer: [samples[median]].as_bytes(),
+                buffer: [median].as_bytes(),
             },
         );
 
