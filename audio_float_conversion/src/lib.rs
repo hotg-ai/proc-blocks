2d0bb602e7eed8acfa4d81a92900e87ba23bf9f5
mohit0928 <mohityadav0928@gmail.com>
2022-05-11 17:25:40 +0530

    updated audio_float_conversion to new abi format

--- audio_float_conversion/src/lib.rs
+++ audio_float_conversion/src/lib.rs
@@ -1,114 +1,148 @@
-#![cfg_attr(not(feature = "metadata"), no_std)]
+use crate::proc_block_v1::{
+    BadInputReason, GraphError, InvalidInput, KernelError,
+};
+use hotg_rune_proc_blocks::{runtime_v1::*, BufferExt, SliceExt};
 
-extern crate alloc;
-
-use hotg_rune_proc_blocks::{ProcBlock, Tensor, Transform};
-
-#[derive(Debug, Clone, PartialEq, ProcBlock)]
-#[transform(inputs = [i16; _], outputs = [f32; _])]
-pub struct AudioFloatConversion {
-    i16_max_as_float: f32,
-}
+wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
 
 const I16_MAX_AS_FLOAT: f32 = i16::MAX as f32;
 
-impl AudioFloatConversion {
-    pub const fn new() -> Self {
-        AudioFloatConversion {
-            i16_max_as_float: I16_MAX_AS_FLOAT,
-        }
-    }
+#[derive(Debug, Clone, PartialEq)]
+struct ProcBlockV1;
 
-    fn check_input_dimensions(&self, dimensions: &[usize]) {
-        assert_eq!(
-            (!(dimensions.len() == 2 && dimensions[0] == 1)
-                || !(dimensions.len() == 1)),
-            true,
-            "This proc block only supports 1D outputs (requested output: {:?})",
-            dimensions
+impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
+    fn register_metadata() {
+        let metadata =
+            Metadata::new("Audio Float Conversion", env!("CARGO_PKG_VERSION"));
+        metadata.set_description(env!("CARGO_PKG_DESCRIPTION"));
+        metadata.set_repository(env!("CARGO_PKG_REPOSITORY"));
+        metadata.set_homepage(env!("CARGO_PKG_HOMEPAGE"));
+        metadata.add_tag("audio");
+        metadata.add_tag("float");
+
+        let input = TensorMetadata::new("input");
+        let hint = supported_shapes(
+            &[ElementType::I16],
+            DimensionsParam::Fixed(&[1, 0]),
         );
+        input.add_hint(&hint);
+        metadata.add_input(&input);
+
+        let output = TensorMetadata::new("output");
+        output.set_description(
+            "converted values from i16 data type to a floating-point value.",
+        );
+        let hint = supported_shapes(
+            &[ElementType::F32],
+            DimensionsParam::Fixed(&[1, 0]),
+        );
+        output.add_hint(&hint);
+        metadata.add_output(&output);
+
+        register_node(&metadata);
+    }
+    fn graph(id: String) -> Result<(), GraphError> {
+        let ctx = GraphContext::for_node(&id).ok_or_else(|| {
+            GraphError::Other("Unable to get the graph context".to_string())
+        })?;
+
+        ctx.add_input_tensor(
+            "input",
+            ElementType::I16,
+            DimensionsParam::Fixed(&[1, 0]),
+        );
+        ctx.add_output_tensor(
+            "max_index",
+            ElementType::F32,
+            DimensionsParam::Fixed(&[1, 0]),
+        );
+
+        Ok(())
+    }
+    fn kernel(id: String) -> Result<(), KernelError> {
+        let ctx = KernelContext::for_node(&id).ok_or_else(|| {
+            KernelError::Other("Unable to get the kernel context".to_string())
+        })?;
+
+        let TensorResult {
+            element_type,
+            dimensions,
+            buffer,
+        } = ctx.get_input_tensor("input").ok_or_else(|| {
+            KernelError::InvalidInput(InvalidInput {
+                name: "input".to_string(),
+                reason: BadInputReason::NotFound,
+            })
+        })?;
+
+        check_input_dimensions(&dimensions);
+
+        let output = match element_type {
+            ElementType::I16 => {
+                audio_float_conversion(buffer.elements::<i16>())
+            },
+
+            other => {
+                return Err(KernelError::Other(format!(
+                "The Audio Float Conversion proc-block only accepts I16 tensors, found {:?}",
+                other,
+                )))
+            },
+        };
+
+        let output = match output {
+            Some(ix) => ix,
+            None => {
+                return Err(KernelError::Other(
+                    "The input tensor was empty".to_string(),
+                ))
+            },
+        };
+
+        let resulting_tensor = output.as_bytes();
+
+        ctx.set_output_tensor(
+            "max_index",
+            TensorParam {
+                element_type: ElementType::F32,
+                dimensions: &dimensions,
+                buffer: &resulting_tensor,
+            },
+        );
+
+        Ok(())
     }
 }
 
-impl Default for AudioFloatConversion {
-    fn default() -> Self { AudioFloatConversion::new() }
-}
-
-impl Transform<Tensor<i16>> for AudioFloatConversion {
-    type Output = Tensor<f32>;
-
-    fn transform(&mut self, input: Tensor<i16>) -> Self::Output {
-        self.check_input_dimensions(input.dimensions());
-        input.map(|_dims, &value| {
-            (value as f32 / I16_MAX_AS_FLOAT).clamp(-1.0, 1.0)
-        })
-    }
-}
-
-#[cfg(feature = "metadata")]
-pub mod metadata {
-    wit_bindgen_rust::import!(
-        "../wit-files/rune/runtime-v1.wit"
+fn check_input_dimensions(dimensions: &[u32]) {
+    assert_eq!(
+        (!(dimensions.len() == 2 && dimensions[0] == 1)
+            || !(dimensions.len() == 1)),
+        true,
+        "This proc block only supports 1D outputs (requested output: {:?})",
+        dimensions
     );
-    wit_bindgen_rust::export!(
-        "../wit-files/rune/rune-v1.wit"
-    );
-
-    struct RuneV1;
-
-    impl rune_v1::RuneV1 for RuneV1 {
-        fn start() {
-            use runtime_v1::*;
-
-            let metadata = Metadata::new(
-                "Audio to Float Conversion",
-                env!("CARGO_PKG_VERSION"),
-            );
-            metadata.set_description(
-                "Convert a tensor of `i16` samples to a normalized tensor of `f32`.",
-            );
-            metadata.set_repository(env!("CARGO_PKG_REPOSITORY"));
-            metadata.set_homepage(env!("CARGO_PKG_HOMEPAGE"));
-            metadata.add_tag("audio");
-
-            let input = TensorMetadata::new("input");
-            input.set_description("A 1D tensor of `i16` samples.");
-            let hint = supported_shapes(
-                &[ElementType::Int16],
-                Dimensions::Fixed(&[0]),
-            );
-            input.add_hint(&hint);
-            metadata.add_input(&input);
-
-            let output = TensorMetadata::new("normalized");
-            output.set_description(
-                "The samples as floats normalized to the range `[0, 1]`.",
-            );
-            let hint = supported_shapes(
-                &[ElementType::Float32],
-                Dimensions::Fixed(&[0]),
-            );
-            output.add_hint(&hint);
-            metadata.add_output(&output);
-
-            register_node(&metadata);
-        }
+}
+fn audio_float_conversion(values: &[i16]) -> Option<Vec<f32>> {
+    let mut output = Vec::new();
+    for i in 0..values.len() {
+        output.push((values[i] as f32 / I16_MAX_AS_FLOAT).clamp(-1.0, 1.0));
     }
+    Some(output)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    extern crate alloc;
     use alloc::vec;
 
     #[test]
     fn handle_empty() {
-        let mut pb = AudioFloatConversion::new();
-        let input = Tensor::new_vector(vec![0; 15]);
-
-        let got = pb.transform(input);
-
-        assert_eq!(got.dimensions(), &[15]);
+        let input = [0; 15];
+        let got = audio_float_conversion(&input).unwrap();
+        let dim = got.len();
+        assert_eq!(dim, 15);
     }
 
     #[test]
@@ -116,23 +150,21 @@ mod tests {
         let max = i16::MAX;
         let min = i16::MIN;
 
-        let mut pb = AudioFloatConversion::new();
-        let input = Tensor::new_vector(vec![0, max / 2, min / 2]);
+        let input = [0, max / 2, min / 2];
 
-        let got = pb.transform(input);
+        let got = audio_float_conversion(&input).unwrap();
 
-        assert_eq!(got.elements()[0..3], [0.0, 0.49998474, -0.50001526]);
+        assert_eq!(got, vec![0.0, 0.49998474, -0.50001526]);
     }
     #[test]
     fn does_clutch_work() {
         let max = i16::MAX;
         let min = i16::MIN;
 
-        let mut pb = AudioFloatConversion::new();
-        let input = Tensor::new_vector(vec![max, min, min + 1]);
+        let input = [max, min, min + 1];
 
-        let got = pb.transform(input);
+        let got = audio_float_conversion(&input).unwrap();
 
-        assert_eq!(got.elements()[0..3], [1.0, -1.0, -1.0]);
+        assert_eq!(got, vec![1.0, -1.0, -1.0]);
     }
 }
