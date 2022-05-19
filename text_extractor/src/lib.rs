f1e9d7cea1ebdf124445ab5ba5701a07ddd7a345
mohit0928 <mohityadav0928@gmail.com>
2022-05-19 14:20:09 +0530

    only converting output to bytes part left

--- text_extractor/src/lib.rs
+++ text_extractor/src/lib.rs
@@ -1,9 +1,5 @@
 use crate::proc_block_v1::*;
-use hotg_rune_proc_blocks::{
-    ndarray::{s, ArrayView3},
-    runtime_v1::{self, *},
-    BufferExt, SliceExt,
-};
+use hotg_rune_proc_blocks::{runtime_v1::*, BufferExt};
 
 wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
 
@@ -95,24 +91,84 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
         let ctx = KernelContext::for_node(&node_id)
             .ok_or(KernelError::MissingContext)?;
 
-        let TensorResult {
-            element_type,
-            dimensions,
-            buffer,
-        } = ctx.get_input_tensor("phrases").ok_or_else(|| {
+        let text = ctx.get_input_tensor("text").ok_or_else(|| {
             KernelError::InvalidInput(InvalidInput {
-                name: "phrases".to_string(),
+                name: "text".to_string(),
                 reason: BadInputReason::NotFound,
             })
         })?;
+
+        let start_logits =
+            ctx.get_input_tensor("start_logits").ok_or_else(|| {
+                KernelError::InvalidInput(InvalidInput {
+                    name: "start_logits".to_string(),
+                    reason: BadInputReason::NotFound,
+                })
+            })?;
+
+        let end_logits =
+            ctx.get_input_tensor("end_logits").ok_or_else(|| {
+                KernelError::InvalidInput(InvalidInput {
+                    name: "end_logits".to_string(),
+                    reason: BadInputReason::NotFound,
+                })
+            })?;
+        match text.element_type {
+                ElementType::U8 =>{
+                     text.buffer.view::<u8>(&text.dimensions)
+                    .map_err(|e| KernelError::InvalidInput(InvalidInput{ name: "text".to_string(), reason: BadInputReason::InvalidValue(e.to_string()) }))?;
+                    }
+                other => {
+                    return Err(KernelError::Other(format!(
+                    "The Object Filter proc-block doesn't support {:?} element type",
+                    other,
+                    )))
+                },
+            };
+        match start_logits.element_type {
+                ElementType::U32 =>{
+                    start_logits.buffer.view::<f32>(&start_logits.dimensions)
+                    .map_err(|e| KernelError::InvalidInput(InvalidInput{ name: "start_logits".to_string(), reason: BadInputReason::InvalidValue(e.to_string()) }))?;
+                    }
+                other => {
+                    return Err(KernelError::Other(format!(
+                    "The Object Filter proc-block doesn't support {:?} element type",
+                    other,
+                    )))
+                },
+            };
+        match end_logits.element_type {
+                ElementType::U32 =>{
+                    end_logits.buffer.view::<f32>(&start_logits.dimensions)
+                    .map_err(|e| KernelError::InvalidInput(InvalidInput{ name: "end_logits".to_string(), reason: BadInputReason::InvalidValue(e.to_string()) }))?;
+                    }
+                other => {
+                    return Err(KernelError::Other(format!(
+                    "The Object Filter proc-block doesn't support {:?} element type",
+                    other,
+                    )))
+                },
+            };
+        let output = transform((
+            text.buffer.elements(),
+            start_logits.buffer.elements(),
+            end_logits.buffer.elements(),
+        ));
+
+        // ctx.set_output_tensor(
+        //     "phrases",
+        //     TensorParam {
+        //         element_type: ElementType::Utf8,
+        //         dimensions: &[output.len() as u32],
+        //         buffer: &output.as_bytes(),
+        //     },
+        // );
 
         Ok(())
     }
 }
 
-fn transform(
-    inputs: (Tensor<u8>, Tensor<u32>, Tensor<u32>),
-) -> Tensor<Cow<'static, str>> {
+fn transform<'a>(inputs: (&[u8], &[u32], &[u32])) -> Vec<Cow<'static, str>> {
     let (text, start_logits, end_logits) = inputs;
 
     let underlying_bytes: &[u8] = text.elements();
@@ -121,8 +177,8 @@ fn transform(
 
     let input_text: Vec<&str> = input_text.lines().collect();
 
-    let start_index: u32 = start_logits.elements()[0];
-    let end_index: u32 = end_logits.elements()[0];
+    let start_index: u32 = start_logits[0];
+    let end_index: u32 = end_logits[0];
     if end_index <= start_index {
         panic!(
             "Start index: {} is greater than or equal to end index: {}",
@@ -146,70 +202,7 @@ fn transform(
 
     let output_text = vec![Cow::Owned(buffer)];
 
-    Tensor::new_vector(output_text)
-}
-
-#[cfg(feature = "metadata")]
-pub mod metadata {
-    wit_bindgen_rust::import!("../wit-files/rune/runtime-v1.wit");
-    wit_bindgen_rust::export!("../wit-files/rune/rune-v1.wit");
-
-    struct RuneV1;
-
-    impl rune_v1::RuneV1 for RuneV1 {
-        fn start() {
-            use runtime_v1::*;
-
-            let metadata =
-                Metadata::new("Text Extractor", env!("CARGO_PKG_VERSION"));
-            metadata.set_description(
-                "Given a body of text and some start/end indices, extract parts of the text (i.e. words/phrases) specified by those indices.",
-            );
-            metadata.set_repository(env!("CARGO_PKG_REPOSITORY"));
-            metadata.set_homepage(env!("CARGO_PKG_HOMEPAGE"));
-            metadata.add_tag("nlp");
-
-            let text = TensorMetadata::new("text");
-            text.set_description("A string of text.");
-            let hint = supported_shapes(
-                &[ElementType::Uint8],
-                Dimensions::Fixed(&[0]),
-            );
-            text.add_hint(&hint);
-            metadata.add_input(&text);
-
-            let start_logits = TensorMetadata::new("start_logits");
-            start_logits.set_description(
-                "The indices for the start of each word/phrase to extract.",
-            );
-            let hint = supported_shapes(
-                &[ElementType::Uint32],
-                Dimensions::Fixed(&[0]),
-            );
-            start_logits.add_hint(&hint);
-            metadata.add_input(&start_logits);
-
-            let end_logits = TensorMetadata::new("end_logits");
-            end_logits.set_description(
-                "The indices for the end of each word/phrase to extract.",
-            );
-            let hint = supported_shapes(
-                &[ElementType::Uint32],
-                Dimensions::Fixed(&[0]),
-            );
-            end_logits.add_hint(&hint);
-            metadata.add_input(&end_logits);
-
-            let phrases = TensorMetadata::new("phrases");
-            phrases.set_description("The phrases that were extracted.");
-            let hint =
-                supported_shapes(&[ElementType::Utf8], Dimensions::Fixed(&[0]));
-            phrases.add_hint(&hint);
-            metadata.add_output(&phrases);
-
-            register_node(&metadata);
-        }
-    }
+    output_text
 }
 
 #[cfg(test)]
@@ -221,14 +214,12 @@ mod tests {
         let bytes: Vec<u8> = "[UNK]\n[UNK]\nuna\n##ffa\n##ble\nworld\n!"
             .as_bytes()
             .to_vec();
-        let bytes = Tensor::new_vector(bytes);
-        let start_index = Tensor::new_vector(vec![2]);
-        let end_index = Tensor::new_vector(vec![4]);
+        // let bytes =(bytes);
+        let start_index = [2_u32];
+        let end_index = [4_u32];
+        let output = transform((&bytes, &start_index, &end_index));
 
-        let mut text_extractor = TextExtractor::default();
-        let output = text_extractor.transform((bytes, start_index, end_index));
-
-        let should_be = Tensor::new_vector(vec![Cow::Borrowed("unaffable")]);
+        let should_be = vec![Cow::Borrowed("unaffable")];
 
         assert_eq!(output, should_be);
     }
