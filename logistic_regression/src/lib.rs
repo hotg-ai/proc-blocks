ba179ed328f07c4abcb9dd75f13ac3a4bec41dfd
mohit0928 <mohityadav0928@gmail.com>
2022-06-03 18:25:51 +0530

    updated logistic regression model as a proc-block

--- logistic_regression/src/lib.rs
+++ logistic_regression/src/lib.rs
@@ -1,5 +1,6 @@
-use linfa::{traits::{Fit, Predict}, Dataset};
-use linfa_datasets::winequality;
+use linfa::{
+    traits::{Fit, Predict},DatasetView,
+};
 use linfa_logistic::LogisticRegression;
 
 use crate::proc_block_v1::{
@@ -7,11 +8,13 @@ use crate::proc_block_v1::{
     InvalidInput, KernelError,
 };
 use hotg_rune_proc_blocks::{
-    common,
-    runtime_v1::{self, *},
-    BufferExt, SliceExt, ndarray::{ArrayBase, ArrayView1, ViewRepr, IxDynImpl, Dim, stack, Axis, Array3, ArrayView, ArrayViewD},
+    ndarray::{Array1,  ArrayView1,
+        ArrayView2,
+    },
+    runtime_v1::*,
+    BufferExt, SliceExt,
 };
-use std::{fmt::Display, str::FromStr};
+
 
 wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
 
@@ -32,37 +35,26 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
         metadata.add_tag("categorical dependent variable");
 
         let x_train = TensorMetadata::new("x_train");
-        let supported_types = 
-            [ElementType::F32]
-        ;
-        let hint = supported_shapes(&supported_types, DimensionsParam::Dynamic);
+        let supported_types = [ElementType::F64];
+        let hint = supported_shapes(&supported_types, DimensionsParam::Fixed(&[0, 0]));
         x_train.add_hint(&hint);
         metadata.add_input(&x_train);
 
         let y_train = TensorMetadata::new("y_train");
-        let hint = supported_shapes(&supported_types, DimensionsParam::Fixed(&[0]));
+        let hint =
+            supported_shapes(&[ElementType::I32], DimensionsParam::Fixed(&[0]));
         y_train.add_hint(&hint);
         metadata.add_input(&y_train);
 
         let x_test = TensorMetadata::new("x_test");
-        let hint = supported_shapes(&supported_types, DimensionsParam::Dynamic);
+        let hint = supported_shapes(&supported_types, DimensionsParam::Fixed(&[0, 0]));
         x_test.add_hint(&hint);
         metadata.add_input(&x_test);
 
         let y_test = TensorMetadata::new("y_test");
-        let supported_types = [
-            ElementType::U8,
-            ElementType::I8,
-            ElementType::U16,
-            ElementType::I16,
-            ElementType::U32,
-            ElementType::I32,
-            ElementType::F32,
-            ElementType::U64,
-            ElementType::I64,
-            ElementType::F64,
-        ];
-        let hint = supported_shapes(&supported_types, DimensionsParam::Fixed(&[0]));
+        let supported_types = [ElementType::I32];
+        let hint =
+            supported_shapes(&supported_types, DimensionsParam::Fixed(&[0]));
         y_test.add_hint(&hint);
         metadata.add_output(&y_test);
     }
@@ -72,7 +64,7 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
             .ok_or(GraphError::MissingContext)?;
 
         let element_type = match ctx.get_argument("element_type").as_deref() {
-            Some("f32") => ElementType::F32,
+            Some("f64") => ElementType::F64,
             Some(_) => {
                 return Err(GraphError::InvalidArgument(InvalidArgument {
                     name: "element_type".to_string(),
@@ -89,13 +81,21 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
             },
         };
 
-        ctx.add_input_tensor("x_train", element_type, DimensionsParam::Dynamic);
+        ctx.add_input_tensor("x_train", element_type, DimensionsParam::Fixed(&[0, 0]));
 
-        ctx.add_input_tensor("y_train", element_type, DimensionsParam::Fixed(&[0]));
+        ctx.add_input_tensor(
+            "y_train",
+            ElementType::I32,
+            DimensionsParam::Fixed(&[0]),
+        );
 
-        ctx.add_input_tensor("x_test", element_type, DimensionsParam::Dynamic);
+        ctx.add_input_tensor("x_test", element_type, DimensionsParam::Fixed(&[0, 0]));
 
-        ctx.add_output_tensor("y_test", element_type, DimensionsParam::Fixed(&[0]));
+        ctx.add_output_tensor(
+            "y_test",
+            ElementType::I32,
+            DimensionsParam::Fixed(&[0]),
+        );
 
         Ok(())
     }
@@ -118,6 +118,7 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
             })
         })?;
 
+
         let x_test = ctx.get_input_tensor("x_test").ok_or_else(|| {
             KernelError::InvalidInput(InvalidInput {
                 name: "x_test".to_string(),
@@ -125,28 +126,86 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
             })
         })?;
 
-        let y_tensor = y_train.buffer.view::<f32>(&y_train.dimensions).and_then(|t| t.into_dimensionality()).map_err(|e| {
-            KernelError::InvalidInput(InvalidInput {
-                name: "bytes".to_string(),
-                reason: BadInputReason::InvalidValue(e.to_string()),
-            })
-        })?;
+        let y_test_dimension = [x_train.dimensions[0], 1];
 
-        let output = transform(
-            x_train.buffer.view::<f32>(&x_train.dimensions).unwrap(),
-            y_tensor,
-            x_test.buffer.view::<f32>(&x_test.dimensions).unwrap(),
+        let y_train = y_train
+            .buffer
+            .view::<i32>(&y_train.dimensions)
+            .and_then(|t| t.into_dimensionality())
+            .map_err(|e| {
+                KernelError::InvalidInput(InvalidInput {
+                    name: "bytes".to_string(),
+                    reason: BadInputReason::InvalidValue(e.to_string()),
+                })
+            })?;
+
+        let x_train = x_train
+            .buffer
+            .view::<f64>(&x_train.dimensions)
+            .and_then(|t| t.into_dimensionality())
+            .map_err(|e| {
+                KernelError::InvalidInput(InvalidInput {
+                    name: "bytes".to_string(),
+                    reason: BadInputReason::InvalidValue(e.to_string()),
+                })
+            })?;
+
+        let x_test = x_test
+            .buffer
+            .view::<f64>(&x_test.dimensions)
+            .and_then(|t| t.into_dimensionality())
+            .map_err(|e| {
+                KernelError::InvalidInput(InvalidInput {
+                    name: "bytes".to_string(),
+                    reason: BadInputReason::InvalidValue(e.to_string()),
+                })
+            })?;
+
+        let output = transform(x_train, y_train, x_test);
+
+        ctx.set_output_tensor(
+            "y_test",
+            TensorParam {
+                element_type: ElementType::I32,
+                dimensions: &y_test_dimension,
+                buffer: &output.to_vec().as_bytes(),
+            },
         );
 
         Ok(())
     }
 }
 
-fn transform(x_train: ArrayViewD<'_, f32>, y_train: ArrayView1<f32>, x_test: ArrayViewD<'_, f32>) -> Vec<f32> {
-    let training_data = Dataset::new(x_train, y_train);
+fn transform<'a>(
+    x_train: ArrayView2<'a, f64>,
+    y_train: ArrayView1<'a, i32>,
+    x_test: ArrayView2<'_, f64>,
+) -> Array1<i32> {
+    let training_data = DatasetView::new(x_train, y_train);
 
     let model = LogisticRegression::default().fit(&training_data).unwrap();
     let prediction = model.predict(&x_test);
 
     prediction
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_model(){
+        let dataset = linfa_datasets::winequality().map_targets(|x| if *x > 6 { 0 } else { 1 });
+        let (train, valid) = dataset.split_with_ratio(0.8);
+        let y_train = train.targets();
+        let x_train = train.records();
+        let x_test = valid.records();
+        let y_test = valid.targets();
+        let y_pred = transform(x_train.view(), y_train.view(), x_test.view());
+
+        println!("{:?}", y_pred);
+
+        assert_eq!(y_pred, y_test);
+        
+    }
 }
