8aec3b5ce098620d20cfa39169b6090f7c8255f5
Michael-F-Bryan <michael@hotg.ai>
2022-06-15 13:33:24 +0800

    Use the runtime's RNG in proc-blocks

--- support/src/guest/mod.rs
+++ support/src/guest/mod.rs
@@ -1,5 +1,8 @@
 //! Types and utilities for implementing a proc-block.
 
+#[macro_use]
+mod macros;
+
 pub(crate) mod bindings;
 mod element_type;
 mod errors;
@@ -22,25 +25,9 @@ pub use self::{
     proc_block::ProcBlock,
 };
 
-/// Tell the runtime that a WebAssembly module contains a proc-block.
-#[macro_export]
-macro_rules! export_proc_block {
-    (metadata: $metadata_func:expr, proc_block: $proc_block:ty $(,)?) => {
-        #[doc(hidden)]
-        #[no_mangle]
-        pub fn __proc_block_metadata() -> $crate::guest::Metadata { $metadata_func() }
+getrandom::register_custom_getrandom!(host_rng);
 
-        #[doc(hidden)]
-        #[no_mangle]
-        pub fn __proc_block_new(
-            args: Vec<$crate::guest::Argument>,
-        ) -> Result<Box<dyn $crate::guest::ProcBlock>, $crate::guest::CreateError> {
-            fn assert_impl_proc_block(_: &impl $crate::guest::ProcBlock) {}
-
-            let proc_block = <$proc_block>::try_from(args)?;
-            assert_impl_proc_block(&proc_block);
-
-            Ok(Box::new(proc_block) as Box<dyn $crate::guest::ProcBlock>)
-        }
-    };
+fn host_rng(buffer: &mut [u8]) -> Result<(), getrandom::Error> {
+    bindings::get_random(buffer);
+    Ok(())
 }
--- support/src/lib.rs
+++ support/src/lib.rs
@@ -8,21 +8,4 @@ mod strings;
 #[cfg(feature = "guest")]
 pub mod guest;
 
-use std::sync::Mutex;
-
 pub use crate::strings::{decode_strings, StringBuilder};
-
-use once_cell::sync::Lazy;
-use rand::{prelude::SmallRng, Rng, SeedableRng};
-
-// Note: getrandom is pulled in by the linfa_logistic crate
-getrandom::register_custom_getrandom!(unsupported_rng);
-
-fn unsupported_rng(buffer: &mut [u8]) -> Result<(), getrandom::Error> {
-    // FIXME: We should probably seed this with something more useful.
-    static RNG: Lazy<Mutex<SmallRng>> =
-        Lazy::new(|| Mutex::new(SmallRng::from_seed(Default::default())));
-
-    RNG.lock().unwrap().fill(buffer);
-    Ok(())
-}
