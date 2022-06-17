ddc2c3ee76bddee69473679140f47540988d82ec
Michael-F-Bryan <michael@hotg.ai>
2022-06-18 00:38:50 +0800

    Migrated label to v2 WIT files

--- label/src/lib.rs
+++ label/src/lib.rs
@@ -1,157 +1,80 @@
-use crate::proc_block_v1::{
-    BadArgumentReason, BadInputReason, GraphError, InvalidArgument,
-    InvalidInput, KernelError,
-};
-use hotg_rune_proc_blocks::{
-    ndarray::ArrayViewD,
-    runtime_v1::{
-        self, ArgumentMetadata, ArgumentType, DimensionsParam, ElementType,
-        GraphContext, KernelContext, Metadata, TensorMetadata, TensorParam,
-        TensorResult,
-    },
-    BufferExt,
+use hotg_rune_proc_blocks::guest::{
+    parse, Argument, ArgumentMetadata, ArgumentType, CreateError, Dimensions,
+    ElementType, Metadata, ProcBlock, RunError, Tensor, TensorConstraint,
+    TensorConstraints, TensorMetadata,
 };
 use line_span::LineSpans;
-use std::{fmt::Debug, ops::Range};
+use std::{fmt::Debug, ops::Range, str::FromStr};
 
-wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
+hotg_rune_proc_blocks::export_proc_block! {
+    metadata: metadata,
+    proc_block: Labels,
+}
 
-struct ProcBlockV1;
-
-impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
-    fn register_metadata() {
-        let metadata = Metadata::new("Label", env!("CARGO_PKG_VERSION"));
-        metadata.set_description(
+fn metadata() -> Metadata {
+    Metadata::new("Label", env!("CARGO_PKG_VERSION"))
+        .with_description(
                 "Using a wordlist, retrieve the label that corresponds to each element in a tensor.",
-            );
-        metadata.set_repository(env!("CARGO_PKG_REPOSITORY"));
-        metadata.set_homepage(env!("CARGO_PKG_HOMEPAGE"));
-        metadata.add_tag("classify");
+            )
+        .with_repository(env!("CARGO_PKG_REPOSITORY"))
+        .with_homepage(env!("CARGO_PKG_HOMEPAGE"))
+        .with_tag("classify")
+        .with_argument(ArgumentMetadata::new("wordlist")
+        .with_hint(ArgumentType::LongString)
+    )
+        .with_argument(ArgumentMetadata::new("fallback")
+        .with_hint(ArgumentType::String)
+        .with_default_value("")
+    )
+    .with_input(TensorMetadata::new("indices").with_description("Indices for labels in the wordlist."))
+    .with_output(TensorMetadata::new("labels").with_description("The corresponding labels."))
+}
 
-        let labels = ArgumentMetadata::new("wordlist");
-        let hint =
-            runtime_v1::supported_argument_type(ArgumentType::LongString);
-        labels.add_hint(&hint);
-        metadata.add_argument(&labels);
+#[derive(Debug, Clone, PartialEq)]
+struct Labels {
+    fallback: String,
+    wordlist: Lines,
+}
 
-        let fallback = ArgumentMetadata::new("fallback");
-        fallback.set_default_value("");
-        fallback
-            .set_description("The label to use if an index is out of bounds");
-        let hint = runtime_v1::supported_argument_type(ArgumentType::String);
-        fallback.add_hint(&hint);
-        metadata.add_argument(&fallback);
-
-        let indices = TensorMetadata::new("indices");
-        indices.set_description("Indices for labels in the wordlist.");
-        let hint = runtime_v1::supported_shapes(
-            &[ElementType::U32],
-            DimensionsParam::Dynamic,
-        );
-        indices.add_hint(&hint);
-        metadata.add_input(&indices);
-
-        let output = TensorMetadata::new("labels");
-        output.set_description("The corresponding labels.");
-        let hint = runtime_v1::supported_shapes(
-            &[ElementType::Utf8],
-            DimensionsParam::Dynamic,
-        );
-        output.add_hint(&hint);
-        metadata.add_output(&output);
-
-        runtime_v1::register_node(&metadata);
+impl ProcBlock for Labels {
+    fn tensor_constraints(&self) -> TensorConstraints {
+        TensorConstraints {
+            inputs: vec![TensorConstraint::new(
+                "indices",
+                ElementType::U32,
+                Dimensions::Dynamic,
+            )],
+            outputs: vec![TensorConstraint::new(
+                "labels",
+                ElementType::Utf8,
+                Dimensions::Dynamic,
+            )],
+        }
     }
 
-    fn graph(node_id: String) -> Result<(), GraphError> {
-        let ctx = GraphContext::for_node(&node_id)
-            .ok_or_else(|| GraphError::MissingContext)?;
+    fn run(&self, inputs: Vec<Tensor>) -> Result<Vec<Tensor>, RunError> {
+        let indices = Tensor::get_named(&inputs, "indices")?.view::<u32>()?;
 
-        let _ = get_wordlist(|n| ctx.get_argument(n))
-            .map_err(GraphError::InvalidArgument)?;
+        let labels = indices.mapv(|ix| {
+            self.wordlist
+                .get(ix as usize)
+                .unwrap_or(self.fallback.as_str())
+        });
 
-        ctx.add_input_tensor(
-            "indices",
-            ElementType::U32,
-            DimensionsParam::Dynamic,
-        );
-        ctx.add_output_tensor(
-            "labels",
-            ElementType::Utf8,
-            DimensionsParam::Dynamic,
-        );
-
-        Ok(())
-    }
-
-    fn kernel(node_id: String) -> Result<(), KernelError> {
-        let ctx = KernelContext::for_node(&node_id)
-            .ok_or_else(|| KernelError::MissingContext)?;
-
-        let wordlist = get_wordlist(|n| ctx.get_argument(n))
-            .map_err(KernelError::InvalidArgument)?;
-
-        let TensorResult {
-            buffer,
-            dimensions,
-            element_type,
-        } = ctx.get_input_tensor("indices").ok_or_else(|| {
-            KernelError::InvalidInput(InvalidInput {
-                name: "indices".to_string(),
-                reason: BadInputReason::NotFound,
-            })
-        })?;
-
-        let indices = match element_type {
-            ElementType::U32 => {
-                buffer.view::<u32>(&dimensions).map_err(|e| {
-                    KernelError::InvalidInput(InvalidInput {
-                        name: "indices".to_string(),
-                        reason: BadInputReason::InvalidValue(e.to_string()),
-                    })
-                })?
-            },
-            _ => todo!(),
-        };
-
-        let fallback = ctx.get_argument("fallback").unwrap_or_default();
-        let serialized_labels = label(indices, &wordlist, &fallback);
-
-        ctx.set_output_tensor(
-            "labels",
-            TensorParam {
-                element_type: ElementType::Utf8,
-                dimensions: &dimensions,
-                buffer: &serialized_labels,
-            },
-        );
-
-        Ok(())
+        Ok(vec![Tensor::from_strings("labels", &labels)])
     }
 }
 
-fn label(
-    indices: ArrayViewD<'_, u32>,
-    wordlist: &Lines,
-    fallback: &str,
-) -> Vec<u8> {
-    let labels = indices.map(|&index| match wordlist.get(index as usize) {
-        Some(label) => label,
-        None => fallback,
-    });
+impl TryFrom<Vec<Argument>> for Labels {
+    type Error = CreateError;
 
-    hotg_rune_proc_blocks::string_tensor_from_ndarray(&labels)
-}
+    fn try_from(args: Vec<Argument>) -> Result<Self, Self::Error> {
+        let wordlist = parse::required_arg(&args, "wordlist")?;
+        let fallback =
+            parse::optional_arg(&args, "fallback")?.unwrap_or_default();
 
-fn get_wordlist(
-    get_argument: impl FnOnce(&str) -> Option<String>,
-) -> Result<Lines, InvalidArgument> {
-    let wordlist = get_argument("wordlist").ok_or_else(|| InvalidArgument {
-        name: "wordlist".to_string(),
-        reason: BadArgumentReason::NotFound,
-    })?;
-
-    Ok(Lines::new(wordlist))
+        Ok(Labels { wordlist, fallback })
+    }
 }
 
 #[derive(Debug, Default, Clone, PartialEq)]
@@ -173,6 +96,14 @@ impl Lines {
     }
 }
 
+impl FromStr for Lines {
+    type Err = std::convert::Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Lines::new(s.to_string()))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -183,12 +114,20 @@ mod tests {
         let wordlist = "zero\none\ntwo\nthree";
         let wordlist = Lines::new(wordlist.to_string());
         let indices = ndarray::aview1(&[2_u32, 0, 1]);
+        let proc_block = Labels {
+            wordlist,
+            fallback: "...".to_string(),
+        };
 
-        let serialized = label(indices.into_dyn(), &wordlist, "...");
+        let got = proc_block
+            .run(vec![Tensor::new("indices", &indices)])
+            .unwrap();
 
-        let expected = ndarray::arr1(&["two", "zero", "one"]).into_dyn();
-        let got = serialized.string_view(&[3]).unwrap();
-        assert_eq!(got, expected);
+        let should_be = vec![Tensor::from_strings(
+            "labels",
+            &ndarray::arr1(&["two", "zero", "one"]),
+        )];
+        assert_eq!(got, should_be);
     }
 
     #[test]
@@ -196,11 +135,17 @@ mod tests {
         let wordlist = "zero\none\ntwo\nthree";
         let wordlist = Lines::new(wordlist.to_string());
         let indices = ndarray::aview1(&[100_u32]);
+        let proc_block = Labels {
+            wordlist,
+            fallback: "...".to_string(),
+        };
 
-        let serialized = label(indices.into_dyn(), &wordlist, "UNKNOWN");
+        let got = proc_block
+            .run(vec![Tensor::new("indices", &indices)])
+            .unwrap();
 
-        let expected = ndarray::arr1(&["UNKNOWN"]).into_dyn();
-        let got = serialized.string_view(&[1]).unwrap();
-        assert_eq!(got, expected);
+        let should_be =
+            vec![Tensor::from_strings("labels", &ndarray::arr1(&["..."]))];
+        assert_eq!(got, should_be);
     }
 }
