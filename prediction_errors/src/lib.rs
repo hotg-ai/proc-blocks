5381bf575c3c55b09b96f029c2a11761d3752a63
Michael-F-Bryan <michael@hotg.ai>
2022-06-20 18:18:11 +0800

    Migrated the prediction_errors proc-block across

--- prediction_errors/src/lib.rs
+++ prediction_errors/src/lib.rs
@@ -1,161 +1,69 @@
+use hotg_rune_proc_blocks::guest::{
+    ElementType, Metadata, ProcBlock, RunError, Tensor, TensorConstraint,
+    TensorConstraints, TensorMetadata,
+};
 use smartcore::metrics::{
     mean_absolute_error::MeanAbsoluteError, mean_squared_error::MeanSquareError,
 };
 
-use crate::proc_block_v1::{
-    BadArgumentReason, BadInputReason, GraphError, InvalidArgument,
-    InvalidInput, KernelError,
-};
-use hotg_rune_proc_blocks::{runtime_v1::*, BufferExt, SliceExt};
-
-wit_bindgen_rust::export!("../wit-files/rune/proc-block-v1.wit");
-
-/// a proc-block to find Mean Absolute Error and Mean Squared Error
-struct ProcBlockV1;
-
-impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
-    fn register_metadata() {
-        let metadata = Metadata::new("Errors", env!("CARGO_PKG_VERSION"));
-        metadata.set_description("for assessing prediction error");
-        metadata.set_repository(env!("CARGO_PKG_REPOSITORY"));
-        metadata.set_homepage(env!("CARGO_PKG_HOMEPAGE"));
-        metadata.add_tag("metric");
-        metadata.add_tag("analytics");
-        metadata.add_tag("loss");
-
-        let y_true = TensorMetadata::new("y_true");
-        let hint =
-            supported_shapes(&[ElementType::F64], DimensionsParam::Fixed(&[0]));
-        y_true.add_hint(&hint);
-        metadata.add_input(&y_true);
-
-        let y_pred = TensorMetadata::new("y_pred");
-        let supported_types = [ElementType::F64];
-        let hint =
-            supported_shapes(&supported_types, DimensionsParam::Fixed(&[0]));
-        y_pred.add_hint(&hint);
-        metadata.add_input(&y_pred);
-
-        let mae = TensorMetadata::new("mean_absolute_error");
-        let supported_types = [ElementType::F64];
-        let hint =
-            supported_shapes(&supported_types, DimensionsParam::Fixed(&[1]));
-        mae.add_hint(&hint);
-        metadata.add_output(&mae);
-
-        let mse = TensorMetadata::new("mean_square_error");
-        let supported_types = [ElementType::F64];
-        let hint =
-            supported_shapes(&supported_types, DimensionsParam::Fixed(&[1]));
-        mse.add_hint(&hint);
-        metadata.add_output(&mse);
-
-        register_node(&metadata);
-    }
-
-    fn graph(node_id: String) -> Result<(), GraphError> {
-        let ctx = GraphContext::for_node(&node_id)
-            .ok_or(GraphError::MissingContext)?;
-
-        let element_type = match ctx.get_argument("element_type").as_deref() {
-            Some("f64") => ElementType::F64,
-            Some(_) => {
-                return Err(GraphError::InvalidArgument(InvalidArgument {
-                    name: "element_type".to_string(),
-                    reason: BadArgumentReason::InvalidValue(
-                        "Unsupported element type".to_string(),
-                    ),
-                }));
-            },
-            None => {
-                return Err(GraphError::InvalidArgument(InvalidArgument {
-                    name: "element_type".to_string(),
-                    reason: BadArgumentReason::NotFound,
-                }))
-            },
-        };
-
-        ctx.add_input_tensor(
-            "y_true",
-            element_type,
-            DimensionsParam::Fixed(&[0]),
-        );
-
-        ctx.add_input_tensor(
-            "y_pred",
-            element_type,
-            DimensionsParam::Fixed(&[0]),
-        );
-
-        ctx.add_output_tensor(
-            "mean_absolute_error",
-            element_type,
-            DimensionsParam::Fixed(&[1]),
-        );
-
-        ctx.add_output_tensor(
-            "mean_square_error",
-            element_type,
-            DimensionsParam::Fixed(&[1]),
-        );
-
-        Ok(())
-    }
-
-    fn kernel(node_id: String) -> Result<(), KernelError> {
-        let ctx = KernelContext::for_node(&node_id)
-            .ok_or(KernelError::MissingContext)?;
-
-        let y_true = ctx.get_input_tensor("y_true").ok_or_else(|| {
-            KernelError::InvalidInput(InvalidInput {
-                name: "y_true".to_string(),
-                reason: BadInputReason::NotFound,
-            })
-        })?;
-
-        let y_pred = ctx.get_input_tensor("y_pred").ok_or_else(|| {
-            KernelError::InvalidInput(InvalidInput {
-                name: "y_pred".to_string(),
-                reason: BadInputReason::NotFound,
-            })
-        })?;
-
-        let metric = transform(
-            y_true.buffer.elements().to_vec(),
-            y_pred.buffer.elements().to_vec(),
-        );
-
-        let mae = vec![metric.0];
-
-        ctx.set_output_tensor(
-            "mean_absolute_error",
-            TensorParam {
-                element_type: ElementType::F64,
-                dimensions: &[1 as u32],
-                buffer: &mae.as_bytes(),
-            },
-        );
-
-        let mse = vec![metric.1];
-
-        ctx.set_output_tensor(
-            "mean_square_error",
-            TensorParam {
-                element_type: ElementType::F64,
-                dimensions: &[1 as u32],
-                buffer: &mse.as_bytes(),
-            },
-        );
-
-        Ok(())
-    }
+hotg_rune_proc_blocks::export_proc_block! {
+    metadata: metadata,
+    proc_block: PredictionErrors,
 }
 
-fn transform(y_true: Vec<f64>, y_pred: Vec<f64>) -> (f64, f64) {
-    let mae = MeanAbsoluteError {}.get_score(&y_pred, &y_true);
-    let mse = MeanSquareError {}.get_score(&y_pred, &y_true);
+fn metadata() -> Metadata {
+    Metadata::new("Errors", env!("CARGO_PKG_VERSION"))
+        .with_description("for assessing prediction error")
+        .with_repository(env!("CARGO_PKG_REPOSITORY"))
+        .with_homepage(env!("CARGO_PKG_HOMEPAGE"))
+        .with_tag("metric")
+        .with_tag("analytics")
+        .with_tag("loss")
+        .with_input(TensorMetadata::new("y_true"))
+        .with_input(TensorMetadata::new("y_pred"))
+        .with_output(TensorMetadata::new("mean_absolute_error"))
+        .with_output(TensorMetadata::new("mean_square_error"))
+}
 
-    (mae, mse)
+/// a proc-block to find Mean Absolute Error and Mean Squared Error
+struct PredictionErrors;
+
+impl ProcBlock for PredictionErrors {
+    fn tensor_constraints(&self) -> TensorConstraints {
+        TensorConstraints {
+            inputs: vec![
+                TensorConstraint::new("y_true", ElementType::F64, [0]),
+                TensorConstraint::new("y_pred", ElementType::F64, [0]),
+            ],
+            outputs: vec![
+                TensorConstraint::new(
+                    "mean_absolute_error",
+                    ElementType::F64,
+                    [1],
+                ),
+                TensorConstraint::new(
+                    "mean_square_error",
+                    ElementType::F64,
+                    [1],
+                ),
+            ],
+        }
+    }
+
+    fn run(&self, inputs: Vec<Tensor>) -> Result<Vec<Tensor>, RunError> {
+        let y_true = Tensor::get_named(&inputs, "y_true")?.view_1d::<f64>()?;
+        let y_pred = Tensor::get_named(&inputs, "y_pred")?.view_1d::<f64>()?;
+
+        let y_pred = y_pred.to_vec();
+        let y_true = y_true.to_vec();
+        let mae = MeanAbsoluteError {}.get_score(&y_true, &y_pred);
+        let mse = MeanSquareError {}.get_score(&y_true, &y_pred);
+
+        Ok(vec![
+            Tensor::new_1d("mean_absolute_error", &[mae]),
+            Tensor::new_1d("mean_square_error", &[mse]),
+        ])
+    }
 }
 
 #[cfg(test)]
@@ -163,21 +71,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_mae() {
-        let y_pred: Vec<f64> = vec![0., 0., 1., 1., 1., 1.];
-        let y_true: Vec<f64> = vec![0., 1., 1., 0., 1., 0.];
-        let metric = transform(y_true, y_pred);
+    fn known_values() {
+        let predict_errors = PredictionErrors;
+        let inputs = vec![
+            Tensor::new_1d("y_pred", &[0.0_f64, 0.0, 1.0, 1.0, 1.0, 1.0]),
+            Tensor::new_1d("y_true", &[0.0_f64, 1.0, 1.0, 0.0, 1.0, 0.0]),
+        ];
 
-        assert_eq!(0.5, metric.0);
-    }
+        let got = predict_errors.run(inputs).unwrap();
 
-    #[test]
-    fn check_mse() {
-        let y_pred: Vec<f64> = vec![0., 0., 1., 1., 1., 1.];
-        let y_true: Vec<f64> = vec![0., 1., 1., 0., 1., 0.];
-
-        let metric = transform(y_true, y_pred);
-
-        assert_eq!(0.5, metric.1);
+        let should_be = vec![
+            Tensor::new_1d("mean_absolute_error", &[0.5]),
+            Tensor::new_1d("mean_square_error", &[0.5]),
+        ];
+        assert_eq!(got, should_be);
     }
 }
