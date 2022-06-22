e81032001095187eec5e66af691c890a33754dff
mohit0928 <mohityadav0928@gmail.com>
2022-06-22 19:57:29 +0530

    added arguement metadata

--- linear_regression/src/lib.rs
+++ linear_regression/src/lib.rs
@@ -25,6 +25,15 @@ impl proc_block_v1::ProcBlockV1 for ProcBlockV1 {
         metadata.add_tag("linear modeling");
         metadata.add_tag("analytics");
 
+        let element_type = ArgumentMetadata::new("element_type");
+        element_type
+            .set_description("The type of tensor this proc-block will accept");
+        element_type.set_default_value("f64");
+        element_type.add_hint(&interpret_as_string_in_enum(&[
+            "u8", "i8", "u16", "i16", "u32", "i32", "f32", "u64", "i64", "f64",
+        ]));
+        metadata.add_argument(&element_type);
+
         let x_train = TensorMetadata::new("x_train");
         let supported_types = [ElementType::F64];
         let hint =
